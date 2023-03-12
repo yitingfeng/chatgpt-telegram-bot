d0d8e7a0a32f536df8fe157b635da82c642dd630
Yiting Feng <yiting.feng@gmail.com>
2023-03-12 00:01:10 +0000

    add s3

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from dotenv import load_dotenv
 from openai_helper import OpenAIHelper
 from telegram_bot import ChatGPT3TelegramBot
 
+from boto.s3.connection import S3Connection
+s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
 
 def main():
     # Read .env file
